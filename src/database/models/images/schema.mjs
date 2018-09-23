182f95282e4abcea09a31a1c95606918a17de8e0
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 19:37:15 +0200

    Update schema to have named sizes instead of just an array

--- src/database/models/images/schema.mjs
+++ src/database/models/images/schema.mjs
@@ -2,6 +2,27 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
+const ImageSizeSchema = {
+  height: {
+    type: Number,
+    required: true
+  },
+  mimeType: {
+    type: String,
+    required: true,
+    enum: ['image/jpeg', 'image/png']
+  },
+  // relative path
+  path: {
+    type: String,
+    required: true
+  },
+  width: {
+    type: Number,
+    required: true
+  }
+};
+
 export default new Schema(
   {
     title: {
@@ -9,26 +30,9 @@ export default new Schema(
       required: true
     },
     sizes: {
-      type: [
-        {
-          height: {
-            type: Number,
-            required: true
-          },
-          mimeType: {
-            type: String,
-            enum: ['image/jpeg', 'image/png']
-          },
-          path: {
-            type: String,
-            required: true
-          },
-          width: {
-            type: Number,
-            required: true
-          }
-        }
-      ]
+      thumbnail: ImageSizeSchema,
+      large: ImageSizeSchema,
+      original: ImageSizeSchema
     }
   },
   {

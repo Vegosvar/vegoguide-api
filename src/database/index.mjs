21c9d053563316117d7faf80f0a91a1e97d207d7
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 19:09:57 +0200

    Add port in mongo connection string

--- src/database/index.mjs
+++ src/database/index.mjs
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import * as models from './models';
 
 export default ({ config }) => {
-  const { host, name } = config.database;
-  const uri = `mongo://${host}/${name}`;
+  const { host, name, port } = config.database;
+  const uri = `mongodb://${host}:${port}/${name}`;
 
   // Register models
   Object.keys(models).forEach(key => {

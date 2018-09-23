7b3ced9cd2e0af7f89bd04a51da32caf80f4bcff
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 02:06:04 +0200

    Set up cors, pass in helpers to routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,14 @@
 import express from "express";
-import config from "../config.mjs";
+import cors from "cors";
+import * as helpers from "./helpers";
 import * as routes from "./routes";
+import config from "../config.mjs";
 
 const app = express();
-const database = {};
+app.use(cors());
 
 // Initialize routes
-Object.keys(routes).forEach(key =>
-  routes[key]({ app, config, database })
-);
+Object.keys(routes).forEach(key => routes[key]({ app, config, helpers }));
 
 app.listen(config.port, () =>
   console.log(`Example app listening on port ${config.port}!`)

ed17486fdce92835e1c8b8037ae25867f5f1c3c2
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 19:13:12 +0200

    Export models

--- src/database/models/index.mjs
+++ src/database/models/index.mjs
@@ -1,1 +1,4 @@
-export { default as Posts } from './posts';
+export { default as comments } from './comments';
+export { default as files } from './files';
+export { default as images } from './images';
+export { default as posts } from './posts';

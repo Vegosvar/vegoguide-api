acfe97fedb90aa3b24cc9bd2ed290c11fcac3f1a
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-01 18:59:01 +0200

    Update users schema to include google/facebook ids, set required properties as just that

--- src/database/models/users/schema.mjs
+++ src/database/models/users/schema.mjs
@@ -5,14 +5,26 @@ const { Schema } = mongoose;
 export default new Schema(
   {
     firstName: {
-      type: String
+      type: String,
+      required: true
     },
     lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String
+    },
+    facebookId: {
+      type: String
+    },
+    googleId: {
       type: String
     },
     roles: {
-      type: Schema.Types.ObjectId,
-      ref: 'roles'
+      type: [Schema.Types.ObjectId],
+      ref: 'roles',
+      required: true
     }
   },
   {

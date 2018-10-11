202e03ce6e9a54aeb954c45e9990025e4346812c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-11 15:59:07 +0200

    Break out ImageSizeSchema into its own file and make it a Mongoose Schema so we can add the virtual 'url' property, which is generated by the getUrl helper and the document's path

---
+++ src/database/models/images/schema/ImageSize/index.mjs
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+export default ({ config, helpers }) => {
+  const schema = new Schema({
+    height: {
+      type: Number,
+      required: true
+    },
+    // relative path
+    path: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: Number,
+      required: true
+    }
+  }, {
+    id: false,
+    _id: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  });
+
+  schema.virtual('url').get(function getUrl() {
+    return helpers.getUrl({
+      ...config.api,
+      path: this.path
+    });
+  });
+
+  return schema;
+}
---
+++ src/database/models/images/schema/index.mjs
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import ImageSizeSchema from './ImageSize';
+
+const { Schema } = mongoose;
+
+export default params => {
+  const imageSizeSchema = ImageSizeSchema(params);
+
+  return new Schema(
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      mimeType: {
+        type: String,
+        required: true,
+        enum: ['image/jpeg', 'image/png']
+      },
+      thumbnail: {
+        type: imageSizeSchema,
+        required: true
+      },
+      original: {
+        type: imageSizeSchema,
+        required: true
+      }
+    },
+    {
+      id: false,
+      timestamps: true
+    }
+  );
+};

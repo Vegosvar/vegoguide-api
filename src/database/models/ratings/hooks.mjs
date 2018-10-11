cfe758c768fa5e27b6812a52c0e16895e6d78636
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-11 16:51:18 +0200

    When a rating is saved, generate the new ratings/rating property for that post

---
+++ src/database/models/ratings/hooks.mjs
@@ -0,0 +1,45 @@
+export default ({ schema }) => {
+  schema.post('save', function postSave(ratingsDocument) {
+    return this.model('ratings')
+      .aggregate([
+        {
+          $match: {
+            post: ratingsDocument.post
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            count: {
+              $sum: 1
+            },
+            sum: {
+              $sum: '$rating'
+            }
+          }
+        },
+        {
+          $project: {
+            ratings: '$count',
+            rating: {
+              $divide: ['$count', '$sum']
+            }
+          }
+        }
+      ])
+      .then(({ ratings, rating }) =>
+        this.model('posts')
+          .findOne({
+            _id: ratingsDocument.post
+          })
+          .then(postDocument => {
+            if (postDocument) {
+              postDocument.ratings = ratings;
+              postDocument.rating = rating;
+
+              return postDocument.save();
+            }
+          })
+      );
+  });
+};
--- src/database/models/ratings/index.mjs
+++ src/database/models/ratings/index.mjs
@@ -1,3 +1,7 @@
+import hooks from './hooks';
 import schema from './schema';
 
-export default ({ mongoose }) => mongoose.model('ratings', schema);
+export default ({ mongoose, ...params }) => {
+  hooks({ schema, ...params });
+  return mongoose.model('ratings', schema);
+};

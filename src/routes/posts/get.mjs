6e9d27b79fda8e029f02fd76170c4a8005c95c8d
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-12 15:27:40 +0200

    Add routes for ratings and comments

--- src/routes/posts/get.mjs
+++ src/routes/posts/get.mjs
@@ -18,7 +18,80 @@ export default ({ app, db, helpers, prefix }) => {
 
     return db
       .model('posts')
-      .find(params)
+      .find({
+        url: req.params.url,
+        ...params
+      })
+      .then((data = []) =>
+        res.json({
+          success: true,
+          data
+        })
+      );
+  });
+
+  app.get(`${prefix}/posts/:url/ratings`, (req, res) => {
+    const params = helpers.parseQuery(req.query);
+
+    return db
+      .model('posts')
+      .findOne(
+        {
+          url: req.params.url
+        },
+        {},
+        {
+          autopopulate: false
+        }
+      )
+      .select({
+        _id: true
+      })
+      .then(post => {
+        if (post) {
+          return db.model('ratings').find({
+            post: post._id,
+            ...params
+          });
+        }
+
+        throw new Error('Post not found');
+      })
+      .then((data = []) =>
+        res.json({
+          success: true,
+          data
+        })
+      );
+  });
+
+  app.get(`${prefix}/posts/:url/comments`, (req, res) => {
+    const params = helpers.parseQuery(req.query);
+
+    return db
+      .model('posts')
+      .findOne(
+        {
+          url: req.params.url
+        },
+        {},
+        {
+          autopopulate: false
+        }
+      )
+      .select({
+        _id: true
+      })
+      .then(post => {
+        if (post) {
+          return db.model('comments').find({
+            post: post._id,
+            ...params
+          });
+        }
+
+        throw new Error('Post not found');
+      })
       .then((data = []) =>
         res.json({
           success: true,
--- src/routes/posts/post.mjs
+++ src/routes/posts/post.mjs
@@ -5,4 +5,18 @@ export default ({ app, prefix }) => {
       data: req.body
     })
   );
+
+  app.post(`${prefix}/posts/:url/rating`, (req, res) =>
+    res.json({
+      success: true,
+      data: req.body
+    })
+  );
+
+  app.post(`${prefix}/posts/:url/comment`, (req, res) =>
+    res.json({
+      success: true,
+      data: req.body
+    })
+  );
 };

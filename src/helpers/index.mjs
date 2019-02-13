3018a0bb1fc777a80775863241ca8d790e2cb627
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-02-14 00:50:34 +0100

    Handle error when attempting to parse value as JSON

--- src/helpers/index.mjs
+++ src/helpers/index.mjs
@@ -1,11 +1,17 @@
 export const parseQuery = query =>
-  Object.keys(query).reduce(
-    (parsedQuery, key) => ({
-      ...parsedQuery,
-      [key]: JSON.parse(query[key])
-    }),
-    {}
-  );
+  Object.keys(query).reduce((parsedQuery, key) => {
+    const newParsedQuery = { ...parsedQuery };
+
+    try {
+      newParsedQuery[key] = JSON.parse(query[key]);
+    } catch (error) {
+      console.error(error);
+
+      newParsedQuery[key] = query[key];
+    }
+
+    return newParsedQuery;
+  }, {});
 
 export const getUrl = ({ host, path, port, protocol, version }) =>
   `${protocol}://${host}${port ? `:${port}` : ''}/${version}/${path}`;

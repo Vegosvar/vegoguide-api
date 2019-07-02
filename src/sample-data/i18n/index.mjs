535420bb3f09d05822985009949799f80eea358d
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 01:24:07 +0200

    Add namespace to sample data

--- src/sample-data/i18n/index.mjs
+++ src/sample-data/i18n/index.mjs
@@ -2,26 +2,31 @@ export default [
   {
     message: 'Swedish',
     translation: 'Svenskt',
-    language: 'sv'
+    language: 'sv',
+    namespace: 'categories'
   },
   {
     message: 'Swedish husmanskost',
     translation: 'Husmanskost',
-    language: 'sv'
+    language: 'sv',
+    namespace: 'categories'
   },
   {
     message: 'Fast food',
     translation: 'Snabbmat',
-    language: 'sv'
+    language: 'sv',
+    namespace: 'categories'
   },
   {
     message: 'Kiosk',
     translation: 'Kiosk',
-    language: 'sv'
+    language: 'sv',
+    namespace: 'categories'
   },
   {
     message: 'Grill',
     translation: 'Grill',
-    language: 'sv'
+    language: 'sv',
+    namespace: 'categories'
   }
 ];

99b7beec8bfd9128ec2a65d9abde90e910ec5c6f
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 00:46:55 +0200

    Add i18n route endpoint to retrieve the translations for a single language

---
+++ src/routes/i18n/get.mjs
@@ -0,0 +1,36 @@
+export default ({ app, db, prefix }) => {
+  app.get(`${prefix}/i18n/:language`, (req, res) => {
+    const { language } = req.params;
+
+    return db
+      .model('i18n')
+      .aggregate([
+        {
+          $match: {
+            language
+          }
+        },
+        {
+          $group : {
+            _id : '$language',
+            documents: {
+              $push: '$$ROOT'
+            }
+          }
+        },
+        {
+          $replaceRoot: {
+            newRoot: {
+              [language]: '$documents'
+            }
+          }
+        }
+      ])
+      .then((data = []) =>
+        res.json({
+          success: true,
+          data
+        })
+      );
+  });
+}
--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,3 +1,4 @@
+export { default as i18n } from './i18n'
 export { default as images } from './images'
 export { default as root } from './root'
 export { default as posts } from './posts'

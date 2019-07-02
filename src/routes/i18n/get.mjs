e07ca3b5abc7f8ccb615411280e8cf2f04cdfd13
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 01:11:36 +0200

    Add a GET endpoint for the /i18n base route to return an array of the available languages

--- src/routes/i18n/get.mjs
+++ src/routes/i18n/get.mjs
@@ -1,4 +1,31 @@
 export default ({ app, db, prefix }) => {
+  app.get(`${prefix}/i18n`, (req, res) => {
+    return db
+      .model('i18n')
+      .aggregate([
+        {
+          $group: {
+            _id: null,
+            languages: {
+              $addToSet: '$language'
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            languages: '$languages'
+          }
+        }
+      ])
+      .then((data = []) =>
+        res.json({
+          success: true,
+          data
+        })
+      );
+  });
+
   app.get(`${prefix}/i18n/:language`, (req, res) => {
     const { language } = req.params;
 

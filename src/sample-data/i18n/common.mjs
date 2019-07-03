4cfc493bd53f045c5556cc6ae5ead64195f6b561
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 15:31:48 +0200

    Add more sample data translations

--- src/sample-data/i18n/common.mjs
+++ src/sample-data/i18n/common.mjs
@@ -12,6 +12,24 @@ export default [
     namespace: 'common'
   },
   {
+    message: 'Search',
+    translation: 'Sök',
+    language: 'sv',
+    namespace: 'common'
+  },
+  {
+    message: 'No posts matched the filters',
+    translation: 'Inga poster matchade filtret',
+    language: 'sv',
+    namespace: 'common'
+  },
+  {
+    message: 'Try again',
+    translation: 'Försök igen',
+    language: 'sv',
+    namespace: 'common'
+  },
+  {
     message: 'Search vegan restaurants and shops',
     translation: 'Sök veganska restaranger och butiker',
     language: 'sv',

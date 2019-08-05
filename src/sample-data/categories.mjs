8a7dc15adb1aa54e3c29563cb48118a3df8c9a83
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 01:30:22 +0200

    Create categories from the i18n post categories messages

--- src/sample-data/categories.mjs
+++ src/sample-data/categories.mjs
@@ -1,8 +1,25 @@
 import { objectId } from './generators';
+import i18n from './i18n';
 
-const categories = ['Swedish', 'Swedish husmanskost', 'Fast food', 'Kiosk', 'Grill'];
+const categoriesMessages = i18n.filter(
+  ({ namespace }) => namespace === 'postCategories'
+);
 
-export default categories.map(category => ({
-  _id: objectId(),
-  title: category
-}));
+const categories = categoriesMessages.reduce(
+  (categoriesByMessage, { _id, language, message }) => {
+    if (!(message in categoriesByMessage)) {
+      // eslint-disable-next-line no-param-reassign
+      categoriesByMessage[message] = {
+        _id: objectId(),
+        title: {}
+      };
+    }
+
+    categoriesByMessage[message].title[language] = _id; // eslint-disable-line no-param-reassign
+
+    return categoriesByMessage;
+  },
+  {}
+);
+
+export default Object.values(categories);

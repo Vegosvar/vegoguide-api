d2f32b8361f7258f1920840059015f65bc5e7096
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 01:29:18 +0200

    Create object id for each locale message

--- src/sample-data/i18n/index.mjs
+++ src/sample-data/i18n/index.mjs
@@ -1,13 +1,19 @@
 import common from './common';
 import languages from './languages';
 import postCategories from './post-categories';
+import { objectId } from '../generators';
 
 const localeMessages = [...common, ...languages, ...postCategories];
 
-const defaultLocaleMessages = localeMessages.reduce(
+export default localeMessages.reduce(
   (allLocales, message) => [
     ...allLocales,
     {
+      _id: objectId(),
+      ...message
+    },
+    {
+      _id: objectId(),
       message: message.message,
       translation: message.message,
       namespace: message.namespace,
@@ -16,5 +22,3 @@ const defaultLocaleMessages = localeMessages.reduce(
   ],
   []
 );
-
-export default [...localeMessages, ...defaultLocaleMessages];

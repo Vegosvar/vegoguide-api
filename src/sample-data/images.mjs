f408c7d5ce7cc5d77c72856ea435c90fb8b201df
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 02:20:22 +0200

    Add URL for images

--- src/sample-data/images.mjs
+++ src/sample-data/images.mjs
@@ -1,4 +1,5 @@
 import { objectId } from './generators';
+import { getUrlFromPath } from './helpers';
 
 export default [
   {
@@ -7,16 +8,19 @@ export default [
     mimeType: 'image/jpeg',
     thumbnail: {
       path: 'images/korvkiosken_thumbnail.jpg',
+      url: getUrlFromPath('images/korvkiosken_thumbnail.jpg'),
       height: 281,
       width: 500
     },
     large: {
       path: 'images/korvkiosken_large.jpg',
+      url: getUrlFromPath('images/korvkiosken_large.jpg'),
       height: 575,
       width: 1024
     },
     original: {
       path: 'images/korvkiosken_original.jpg',
+      url: getUrlFromPath('images/korvkiosken_original.jpg'),
       height: 1123,
       width: 2000
     }
@@ -27,16 +31,19 @@ export default [
     mimeType: 'image/jpeg',
     thumbnail: {
       path: 'images/solrosen_thumbnail.jpg',
+      url: getUrlFromPath('images/solrosen_thumbnail.jpg'),
       height: 333,
       width: 500
     },
     large: {
       path: 'images/solrosen_large.jpg',
+      url: getUrlFromPath('images/solrosen_large.jpg'),
       height: 682,
       width: 1024
     },
     original: {
       path: 'images/solrosen_original.jpg',
+      url: getUrlFromPath('images/solrosen_original.jpg'),
       height: 1333,
       width: 2000
     }

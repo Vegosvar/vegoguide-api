91eb8b81f288ed67c18c09a5f1b10e5036f65dbc
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 02:23:27 +0200

    Change url property to be a part of the schema proper instead of generating it as a virtual property

--- src/database/models/images/schema/ImageSize/index.mjs
+++ src/database/models/images/schema/ImageSize/index.mjs
@@ -2,35 +2,29 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-export default ({ config, helpers }) => {
-  const schema = new Schema({
-    height: {
-      type: Number,
-      required: true
+export default () =>
+  new Schema(
+    {
+      height: {
+        type: Number,
+        required: true
+      },
+      // relative path
+      path: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        required: true
+      },
+      width: {
+        type: Number,
+        required: true
+      }
     },
-    // relative path
-    path: {
-      type: String,
-      required: true
-    },
-    width: {
-      type: Number,
-      required: true
+    {
+      id: false,
+      _id: false
     }
-  }, {
-    id: false,
-    _id: false,
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  });
-
-  schema.virtual('url').get(function getUrl() {
-    return helpers.getUrl({
-      ...config.api,
-      ...config.api.public,
-      path: this.path
-    });
-  });
-
-  return schema;
-}
+  );

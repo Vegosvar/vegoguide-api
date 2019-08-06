745d55fb1a110f11ad3a2543d490ca7ea1128a02
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 02:48:11 +0200

    Remove unused helper getTranslations

--- src/sample-data/helpers/index.mjs
+++ src/sample-data/helpers/index.mjs
@@ -2,6 +2,5 @@ export { default as getPostCategory } from './get-post-category';
 export { default as getRandomElementInArray } from './get-random-element-in-array';
 export { default as getRandomElementsInArray } from './get-random-elements-in-array';
 export { default as getRandomNumberInRange } from './get-random-number-in-range';
-export { default as getTranslations } from './get-translations';
 export { default as getTranslationMessages } from './get-translation-messagess';
 export { default as getUrlFromPath } from './get-url-from-path';

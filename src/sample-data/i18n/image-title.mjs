a05c9e4a34c37f12c77dc9e573b2a1b206e44578
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 02:18:59 +0200

    Add translations for image titles

---
+++ src/sample-data/i18n/image-title.mjs
@@ -0,0 +1,32 @@
+export default [
+  {
+    message: 'The hot dog stand',
+    translation: 'Korvkiosken',
+    language: 'sv',
+    namespace: 'imageTitle'
+  },
+  {
+    message: 'Solrosen\'s outdoor sitting area',
+    translation: 'Solrosens uteservering',
+    language: 'sv',
+    namespace: 'imageTitle'
+  },
+  {
+    message: 'Auburgine stew',
+    translation: 'Auburginegryta',
+    language: 'sv',
+    namespace: 'imageTitle'
+  },
+  {
+    message: 'Interior',
+    translation: 'Interiör',
+    language: 'sv',
+    namespace: 'imageTitle'
+  },
+  {
+    message: 'Tasty burger with potato sides',
+    translation: 'Smaskig burgare med potatisklyftor',
+    language: 'sv',
+    namespace: 'imageTitle'
+  },
+];
--- src/sample-data/i18n/index.mjs
+++ src/sample-data/i18n/index.mjs
@@ -1,9 +1,15 @@
 import common from './common';
+import imageTitle from './image-title';
 import languages from './languages';
 import postCategories from './post-categories';
 import { objectId } from '../generators';
 
-const localeMessages = [...common, ...languages, ...postCategories];
+const localeMessages = [
+  ...common,
+  ...imageTitle,
+  ...languages,
+  ...postCategories
+];
 
 export default localeMessages.reduce(
   (allLocales, message) => [

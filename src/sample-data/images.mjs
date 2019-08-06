f56c8f1d6d525978ba762e12c3d91931f21f4f9b
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 02:22:20 +0200

    Add an additional image for solrosen and add blackbird's images

--- src/sample-data/images.mjs
+++ src/sample-data/images.mjs
@@ -47,5 +47,74 @@ export default [
       height: 1333,
       width: 2000
     }
+  },
+  {
+    _id: objectId(),
+    title: getTranslations('Auburgine stew'),
+    mimeType: 'image/jpeg',
+    thumbnail: {
+      path: 'images/solrosen_2_thumbnail.jpg',
+      url: getUrlFromPath('images/solrosen_2_thumbnail.jpg'),
+      height: 281,
+      width: 500
+    },
+    large: {
+      path: 'images/solrosen_2_large.jpg',
+      url: getUrlFromPath('images/solrosen_2_large.jpg'),
+      height: 575,
+      width: 1024
+    },
+    original: {
+      path: 'images/solrosen_2_original.jpg',
+      url: getUrlFromPath('images/solrosen_2_original.jpg'),
+      height: 562,
+      width: 1000
+    }
+  },
+  {
+    _id: objectId(),
+    title: getTranslations('Interior'),
+    mimeType: 'image/jpeg',
+    thumbnail: {
+      path: 'images/blackbird_thumbnail.jpg',
+      url: getUrlFromPath('images/blackbird_thumbnail.jpg'),
+      height: 281,
+      width: 500
+    },
+    large: {
+      path: 'images/blackbird_large.jpg',
+      url: getUrlFromPath('images/blackbird_large.jpg'),
+      height: 575,
+      width: 1024
+    },
+    original: {
+      path: 'images/blackbird_original.jpg',
+      url: getUrlFromPath('images/blackbird_original.jpg'),
+      height: 1123,
+      width: 2000
+    }
+  },
+  {
+    _id: objectId(),
+    title: getTranslations('Tasty burger with potato sides'),
+    mimeType: 'image/jpeg',
+    thumbnail: {
+      path: 'images/blackbird_2_thumbnail.jpg',
+      url: getUrlFromPath('images/blackbird_2_thumbnail.jpg'),
+      height: 281,
+      width: 500
+    },
+    large: {
+      path: 'images/blackbird_2_large.jpg',
+      url: getUrlFromPath('images/blackbird_2_large.jpg'),
+      height: 575,
+      width: 1024
+    },
+    original: {
+      path: 'images/blackbird_2_original.jpg',
+      url: getUrlFromPath('images/blackbird_2_original.jpg'),
+      height: 1123,
+      width: 2000
+    }
   }
 ];
